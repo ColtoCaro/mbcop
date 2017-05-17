c2afbb3ca861405c4d535201423d1c57392e4b22
Jonathon O'Brien <obrienj@colorado.edu>
2017-05-17 18:39:57 -0400

    Made the HROC function

--- R/plotting.R
+++ R/plotting.R
@@ -15,12 +15,58 @@
 #' @param dat An NxP data matrix where the P columns are going to be clustered.
 #'
 #' @param labelID A vector of length P giving integer or text labels for each
-#'  column.
+#'  column.  NA's are allowed which results in the ROC plot being made
+#'  based on the clustering of the full data but only using the non-NA labels
+#'  in computing sensitivity and specificity.
 #'
 #'
 hroc <- function(dat, labelID, ...){
+  #Columns will be clustered.  Accepts options for the Agnes function.
   #First reduce the data for efficient processing
-  smalldat <- ldr(dat)
+  if(nrow(dat) > ncol(dat)){
+  dat <- ldr(dat)
+  }
 
+  hc <- agnes(t(dat), ...)
 
+  mergeTable <- hc$merge
+  distVec <- hc$height[order(hc$height)]
+
+  #create vector for observations with known labels
+  if(sum(is.na(labelID)) > 0){
+    realObs <- (1:length(labelID))[-which(is.na(labelID))]
+    realLabs <- labelID[-which(is.na(labelID))]
+  }else{
+    realObs <- (1:length(labelID))
+    realLabs <- labelID
+  }
+  #create matrix with all possible pairs and a linkage vector
+  pairMat <- t(combn(realObs, 2))
+  linked <- labelID[pairMat[ , 1]] == labelID[pairMat[ , 2]]
+
+  #Finally get the distance at which each point merged
+  pairDist <- apply(pairMat, 1, function(x)
+    getDist(x[1], x[2], mergeTable, distVec))
+
+  pred <- ROCR::prediction(-pairDist, linked)
+  perf <- ROCR::performance(pred, measure = "tpr", x.measure = "fpr")
+
+  plot(perf, col="red", main = paste("HROC with", hc$method, "linkage"))
+  df <- data.frame(pairMat, linked, pairDist)
+  df
 }
+
+
+getDist <- function(x1, x2, mergeTable, distVec){
+  #Function to find the distance at which two points (x1, x2) are merged
+  i <- 1
+  loc1 <- -1*x1
+  loc2 <- -1*x2
+  while(loc1 != loc2){
+    if(mergeTable[i, 1] == loc1 | mergeTable[i, 2] == loc1){loc1 <- i}
+    if(mergeTable[i, 1] == loc2 | mergeTable[i, 2] == loc2){loc2 <- i}
+    if(loc1 == loc2){return(distVec[i])}
+    i <- i + 1
+  }
+}
+
